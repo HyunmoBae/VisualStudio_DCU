e2b52926e590e511c04b84862f3781c74c3db674
현모 배 <tsi0520@cu.ac.kr>
2021-03-26 11:22:36 +0900

    2-12예제

---
+++ 2-12/소스.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+	struct employee {
+		char name[10];
+		int year;
+		int pay;
+	};
+
+int main(void) {
+
+	int i;
+	struct employee LEE[4] = {
+		{"이진호",2014,4200},
+		{"이한영",2015,3300},
+		{"이상원",2015,3500},
+		{"이상범",2016,2900}
+	};
+
+	for (i = 0; i < 4; i++) {
+		printf("\n이름 : %s", LEE[i].name);
+		printf("\n입사 : %d", LEE[i].year);
+		printf("\n연봉 : %d\n", LEE[i].pay);
+
+	}
+	getchar();
+
+}
