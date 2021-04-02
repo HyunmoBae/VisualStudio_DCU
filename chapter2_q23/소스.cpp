3c722bffc912db13f4043bf96488c3e80595568b
현모 배 <tsi0520@cu.ac.kr>
2021-04-02 14:44:15 +0900

    챕터2 과제

--- chapter2_q23/소스.cpp
+++ chapter2_q23/소스.cpp
@@ -6,65 +6,72 @@ struct addressbook {
 	char name[12];
 	char phoneNum[15];
 	char address[30];
-	char birthday[15];
+	char birthday[20];
 };
 
 int main(void) {
-	int i,q,y,d,j= 0;
-	char name2[15];
-	struct addressbook Addbook[100];
+	int i, q, y, d, k,j = 0;
+	char fname[15];
+	struct addressbook Addbook[100] = { NULL };
 
 	while (true) {
-		printf("\t\t\t기능을 선택하세요\n\n");
-		printf("1번 : 목록 보기\t2번 : 추가하기\t3번 : 탐색하기\t4번 : 삭제하기\n");
+		printf("\n\n\t\t\t기능을 선택하세요\n\n");
+		printf("1번 : 목록보기\n2번 : 추가하기\n3번 : 탐색하기\n4번 : 삭제하기\n");
 		scanf_s("%d", &q);
 		switch (q) {
 
 		case 1:
-			printf("이름\t\t\t전화번호\t\t\t\t주소\t\t\t생일\n");
+			printf("\n\n이름\t\t\t\t전화번호\t\t\t주소\t\t\t생일\n");
 			for (y = 0; y <= j - 1; y++) {
-				printf("%7s\t\t\t", Addbook[y].name);
-				printf("%13s\t\t   ", Addbook[y].phoneNum);
-				printf("%13s\t\t", Addbook[y].address);
-				printf("%10s\n", Addbook[y].birthday);
+				printf("%-11s\t\t\t", Addbook[y].name);
+				printf("%-13s\t\t\t", Addbook[y].phoneNum);
+				printf("%-13s\t\t", Addbook[y].address);
+				printf("%-15s\n", Addbook[y].birthday);
 			}
 			continue;
 		case 2:
-			printf("이름 : ");
-			scanf_s("%s", Addbook[j].name, sizeof(Addbook[j].name));
+			printf("\n\n이름 : ");
+			getchar();
+			gets_s(Addbook[j].name);
 			printf("전화번호 : ");
-			scanf_s("%s", Addbook[j].phoneNum, sizeof(Addbook[j].phoneNum));
+			gets_s(Addbook[j].phoneNum);
 			printf("주소 : ");
-			scanf_s("%s", Addbook[j].address, sizeof(Addbook[j].address));
+			gets_s(Addbook[j].address);
 			printf("생일 : ");
-			scanf_s("%s", Addbook[j].birthday, sizeof(Addbook[j].birthday));
+			gets_s(Addbook[j].birthday);
 			j++;
 			continue;
 		case 3:
-			printf("찾을 이름을 검색하세요\n");
-			scanf_s("%s",name2,sizeof(name2[0]));
+			printf("\n찾을 이름을 검색하세요 : ");
+			getchar();
+			gets_s(fname);
 			for (i = 0; i < j; i++) {
-				if (strcmp(name2, Addbook[i].name) == 0) {
-					printf("이름\t\t\t전화번호\t\t\t\t주소\t\t\t생일\n");
-					printf("%7s\t\t\t", Addbook[i].name);
-					printf("%13s\t\t   ", Addbook[i].phoneNum);
-					printf("%13s\t\t", Addbook[i].address);
-					printf("%10s\n", Addbook[i].birthday);
+				if (strcmp(fname, Addbook[i].name) == 0) {
+					printf("\n\n이름\t\t\t\t전화번호\t\t\t주소\t\t\t생일\n");
+					printf("%-11s\t\t\t", Addbook[i].name);
+					printf("%-13s\t\t\t", Addbook[i].phoneNum);
+					printf("%-13s\t\t", Addbook[i].address);
+					printf("%-15s\n", Addbook[i].birthday);
+					break;
 				}
-				else
-					printf("해당 이름은 없습니다.\n");
-				break;
+			}
+			if (strcmp(fname, Addbook[i].name) != 0) {
+				printf("\n해당 이름은 없습니다.\n");
 			}
 			continue;
 		case 4:
-			/*printf("삭제할 행을 선택하세요 : ");
-			scanf("%d", &d);
-			for (i = d; i < j; i++) {
-				Addbook[i - 1].name = Addbook[i].name;
-				Addbook[i - 1].address = Addbook[i].address;
-				j--;*/
-			continue;
-		}
+			printf("\n\n삭제할 이름을 입력하세요 : ");
+			getchar();
+			gets_s(fname);
+			for (i = 0; i < j; i++) {
+				if (strcmp(fname, Addbook[i].name) == 0) {
+					Addbook[i] = { NULL };
+					for(i;i < j; i++)
+					Addbook[i] = Addbook[i + 1];
+				}
+				continue;
+			}
 
+		}
 	}
 }
--- 연습/소스.cpp
+++ 연습/소스.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 int main(void) {
+	char a[5] = "ÇÑ±¹";
+	printf("%10s",a);
 
-	int a[3][4];
-	int b[4][3];
-
-	int i;
-	for()
 
 }
